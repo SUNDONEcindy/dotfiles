74faffdb172093b5fe1e3f7d1c9433563a0ce535
John Farina <johnfarina@gmail.com>
2020-07-06 13:33:40 -1000

    actually only set font to menlo on macos

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(when (symbol-name system-type)
+(when (string-match "darwin" (symbol-name system-type))
   (setq doom-font (font-spec :family "Menlo" :size 15)))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
