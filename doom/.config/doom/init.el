6ddb561b601de90336d8d48304f8df5dfc66b0fb
John Farina <johnfarina@gmail.com>
2024-12-05 13:56:44 -1000

    configure latex

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -172,7 +172,11 @@
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex
+        +latexmk
+        +lsp
+        +fold
+        +cdlatex)              ; writing papers in Emacs has never been so fun
        ;;lean
        ;;ledger            ; an accounting system in Emacs
        lua               ; one-based indices? one-based indices
