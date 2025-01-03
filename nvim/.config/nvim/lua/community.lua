e110dce92cd8dc83606ff184781c1eef58746e49
John Farina <johnfarina@gmail.com>
2025-01-03 08:05:37 -1000

    switch to cattpuccin and add some more community packs

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -9,14 +9,29 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
 
-  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+
+  { import = "astrocommunity.color.nvim-highlight-colors" },
+
+  { import = "astrocommunity.code-runner.vim-slime" },
+  -- { import = "astrocommunity.code-runner.molten-nvim" },
 
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.undotree" },
 
+  -- { import = "astrocommunity.file-explorer.mini-files" },
+
+  { import = "astrocommunity.git.neogit" },
+
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.motion.mini-surround" },
+
   -- packs
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.julia" },
@@ -24,7 +39,18 @@ return {
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.quarto" },
   -- { import = "astrocommunity.pack.sql" },
+  -- { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
+
+  { import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  -- { import = "astrocommunity.programming-language-support.rest-nvim" },
+
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.vscode" },
+
+  { import = "astrocommunity.snippet.nvim-snippets" },
 }
